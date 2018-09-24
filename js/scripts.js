1ed381c3599dc9c0a49f2c2d5702f93ec573a6c7
Fogelion <finoleggen@gmail.com>
2018-09-24 16:45:06 +0300

    add Sormovo.start

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,27 @@
-$(document).ready(function() {
-
-  var switcher = function(btn) {
-    if (btn == "btn_about") {
-      var removeLeft = "normal_side hide_side";
-      var removeCentral = "left_central normal_central";
-      var removeRight = "normal_side hide_side";
-      var addLeft = "wide_side";
-      var addCentral = "right_central";
-      var addRight = "hide_side";
-    } else if (btn == "btn_works") {
-      var removeLeft = "normal_side wide_side";
-      var removeCentral = "normal_side right_central";
-      var removeRight = "normal_side hide_side";
-      var addLeft = "hide_side";
-      var addCentral = "left_central";
-      var addRight = "wide_side";
-    } else if (btn == "logo") {
-      var removeLeft = "wide_side hide_side";
-      var removeCentral = "left_central right_central";
-      var removeRight = "wide_side hide_side";
-      var addLeft = "normal_side";
-      var addCentral = "normal_central";
-      var addRight = "normal_side";
+// $(document).ready(function() {
+$(function() {
+  let switcher = function(btn) {
+    if (btn === "btn_about") {
+			var removeLeft = "normal_side hide_side";
+			var removeCentral = "left_central normal_central";
+			var removeRight = "normal_side hide_side";
+			var addLeft = "wide_side";
+			var addCentral = "right_central";
+			var addRight = "hide_side";
+    } else if (btn === "btn_works") {
+			var removeLeft = "normal_side wide_side";
+			var removeCentral = "normal_side right_central";
+			var removeRight = "normal_side hide_side";
+			var addLeft = "hide_side";
+			var addCentral = "left_central";
+			var addRight = "wide_side";
+    } else if (btn === "logo") {
+			var removeLeft = "wide_side hide_side";
+			var removeCentral = "left_central right_central";
+			var removeRight = "wide_side hide_side";
+			var addLeft = "normal_side";
+			var addCentral = "normal_central";
+			var addRight = "normal_side";
     }
     $("#info").removeClass(removeLeft).addClass(addLeft);
     $("#navigation").removeClass(removeCentral).addClass(addCentral);
